2c8dc3f18624f97362ba08deb24d6c96c0134f29
Bskn1412 <148748232+Bskn1412@users.noreply.github.com>
2024-04-19 22:39:23 +0530

    Update send_email.py

--- email/email/send_email.py
+++ email/email/send_email.py
@@ -1,8 +1,8 @@
 from flask import Flask, jsonify, request
-from flask_cors import CORS
+from flask_cors import CORS, cross_origin # type: ignore
 from mail_notes import send_notes_email
 
-app = Flask(_name_)
+app = Flask(__name__)
 CORS(app)  
 
 @app.route('/submit/<email>/<notes>', methods=['POST', 'GET'])
@@ -14,64 +14,6 @@ def submit(email, notes):
         else:
             return jsonify({"key": "not sent"})
 
-if _name_ == "_main_":
+if __name__ == "_main_":
     app.run(debug=True)
     
-
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-    
-    #@app.route('/check',methods=['POST','GET'])
-#def check():
- #   test = ''.join(list(request.form["n1"]+request.form["n2"]+request.form["n3"]+request.form["n4"]+request.form["n5"]+request.form["n6"])) 
- #   otp = request.form['otp']
- #   user = request.form['user']
- #   if test == otp:
- #       return render_template('home.html',rel=user)
-  #  return render_template("2_step.html",val="re-enter")
-#if __name__ == "__main__":
